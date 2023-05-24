7e3fc9097e650fc45d12eac0a75c0036af201151
priyanka123nayak <nayakpriyanka936@gmail.com>
2023-05-24 16:20:04 +0530

    routes created

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,38 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 import { fetchDataForApi } from "./utils/api";
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration } from "./store/homeSlice";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
+import Explore from "./pages/explore/Explore";
+import SearchResult from "./pages/searchResult/SearchResult";
+import PagenotFound from "./pages/404/PagenotFound";
 
 function App() {
+  const { url } = useSelector((state) => state.home);
+  const dispatch = useDispatch();
+
   useEffect(() => {
     apitesting();
   }, []);
   const apitesting = () => {
     fetchDataForApi("/movie/popular").then((res) => {
       console.log("res: ", res);
+      dispatch(getApiConfiguration(res));
     });
   };
   return (
-    <>
-      <div style={{ color: "white" }}>React</div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details/>} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="*" element={<PagenotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/404/PagenotFound.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./style.scss";
+function PagenotFound() {
+  return <div>PagenotFound</div>;
+}
+
+export default PagenotFound;
