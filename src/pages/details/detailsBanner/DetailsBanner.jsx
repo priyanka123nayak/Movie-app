76ecf9fcd7c12e6710b38621a1f3a5fa60a45fd1
priyanka123nayak <nayakpriyanka936@gmail.com>
2023-06-01 11:57:50 +0530

    complete cast section

--- src/pages/details/detailsBanner/DetailsBanner.jsx
+++ src/pages/details/detailsBanner/DetailsBanner.jsx
@@ -12,8 +12,12 @@ import CircleRating from "../../../components/circleRating/CircleRating";
 import Img from "../../../components/lazyLoadImage/Img.jsx";
 import PosterFallback from "../../../assets/no-poster.png";
 import { PlayIcon } from "../PlayBtn";
+import VideoPopup from "../../../components/videoPopup/VideoPopUp";
 
 const DetailsBanner = ({ video, crew }) => {
+  console.log("video",video)
+  const [show, setShow] = useState();
+  const [videoId, setVideoId] = useState();
   const { mediaType, id } = useParams();
   const { data, loading } = useFetch(`/${mediaType}/${id}`);
   const { url } = useSelector((state) => state.home);
@@ -64,7 +68,13 @@ const DetailsBanner = ({ video, crew }) => {
                     <Genres data={_genres} />
                     <div className="row">
                       <CircleRating rating={data?.vote_average.toFixed(1)} />
-                      <div className="playbtn">
+                      <div
+                        className="playbtn"
+                        onClick={() => {
+                          setShow(true);
+                          setVideoId(video.key);
+                        }}
+                      >
                         <PlayIcon />
                         <span className="text">Watch Trailer</span>
                       </div>
@@ -139,6 +149,12 @@ const DetailsBanner = ({ video, crew }) => {
                     )}
                   </div>
                 </div>
+                <VideoPopup
+                  show={show}
+                  setShow={setShow}
+                  videoId={videoId}
+                  setVideoId={setVideoId}
+                ></VideoPopup>
               </ContentWrapper>
             </>
           )}
